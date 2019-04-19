dc676445628f6bc0fb89e82c4396dcc1b0467369
Haiyang Wang <haiyangwang@ucmerced-10-34-55-176.ucmerced.edu>
2019-04-19 14:24:01 -0700

    s

--- DNA_obj_detction/codes/p1.py
+++ DNA_obj_detction/codes/p1.py
@@ -1,2 +1,17 @@
-def p1():
-	print("hey, this is p1")
+# contour feature analysis
+# https://docs.opencv.org/3.4/dd/d49/tutorial_py_contour_features.html
+
+import cv2
+import numpy as np
+
+img = cv2.imread('../images/isolated_images/6.jpg', 0)
+
+ret, thresh = cv2.threshold(img, 127, 255, 0)
+contours, hierarchy = cv2.findContours(thresh, 1, 2)
+
+cnt = contours[0]
+M = cv2.moments(cnt)
+print(M)
+
+
+
