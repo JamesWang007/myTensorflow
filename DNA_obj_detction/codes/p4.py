cb4ac325f218ad639a5373e59bfafb2a1d13685f
JamesWang007 <wanghaiyang20@gmail.com>
2019-05-04 21:28:59 -0700

    .

---
+++ DNA_obj_detction/codes/p4.py
@@ -0,0 +1,24 @@
+'''
+    load files from a folder, and sort files by file name
+
+'''
+
+import glob
+import re
+import cv2
+
+
+
+file_folder = '../images/isolated_images/'
+files =glob.glob1(file_folder, '*.jpg')
+# if you want sort files according to the digits included in the filename,
+# you can do as following:
+files = sorted(files, key=lambda x:float(re.findall("(\d+)", x)[0]))
+
+index = 0
+for f in files:
+    img_name = cv2.imread(file_folder + f)
+    cv2.imshow(str(index), img_name)
+    cv2.waitKey(0)
+    index += 1
+
--- DNA_obj_detction/shape-detection/LoadImages.py
+++ DNA_obj_detction/shape-detection/LoadImages.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import glob
+import re
 import cv2
 
 class LoadImages:
@@ -10,14 +11,31 @@ class LoadImages:
     def load(self, folder=None):
 
         if folder == None:
-            folder = '../images/isolated_images/*.jpg'
+            folder = '../images/isolated_images/'
 
-        image_path = folder + '*.jpg'
         image_list = []
+
+        '''
+        image_path = folder + '*.jpg'
+        
         for filename in glob.glob(image_path):
             #im = Image.open(filename)
             im = cv2.imread(filename)  # using opencv2
             image_list.append(im)
+        '''
 
+        # sort the files by file name
+        files = glob.glob1(folder, '*.jpg')
+        # if you want sort files according to the digits included in the filename,
+        # you can do as following:
+        files = sorted(files, key=lambda x: float(re.findall("(\d+)", x)[0]))
+
+        for f in files:
+            im = cv2.imread(folder + f)
+            image_list.append(im)
+
+            #cv2.imshow(f, img_name)
+            #cv2.waitKey(0)
         return image_list
 #print(len(image_list))
+
